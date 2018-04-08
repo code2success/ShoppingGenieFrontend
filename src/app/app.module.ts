27d563f774555288f2f0cecce13a1d0dfed2e538
Amal <amal_antony@yahoo.com>
2018-04-08 22:13:02 +1000

    initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import{LoginService} from './login.service'
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule 
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  login : FormGroup;
+  message :string = "";
+  constructor(private fb : FormBuilder, private loginService : LoginService) {
+    this.createForm();
+   }
+
+  ngOnInit() {
+  }
+
+  createForm() {
+    this.login = this.fb.group({
+      username: '',
+      password: ''
+    });
+  }
+  loginPressed(){
+    console.log("Login button is pressed");
+    console.log(this.login.get("username").value);
+    console.log(this.login.get("password").value);
+    let loggedin =  this.loginService.login(this.login.get("username").value,this.login.get("password").value);
+    if(loggedin == true){
+      this.message = "logged in ";
+
+    }else
+    {
+      this.message = "wrong username or password";
+    }
+  }
+  cancelPressed(){
+    this.message = "";
+    this.login.get("username").setValue("");
+    this.login.get("password").setValue("");
+  }
+  }
+
+
